46ab24a47257a1e78d0915ea54c5776aa76c6f90
Julien Lerouge <julien.lerouge.mp@gmail.com>
2014-08-28 15:22:05 +0200

    Fixed an error in setup script

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ from setuptools import setup
 DISTNAME            = 'crino'
 DESCRIPTION         = "Crino: a neural-network library based on Theano"
 LONG_DESCRIPTION    = """Crino is a neural-network library based on Theano, that lets you hand-craft neural-network architectures using a modular framework. It also provides standard implementations for common architectures, such as MLP, DNN, and IODA which is our contribution to the neural-network community."""
-MAINTAINER          = 'Julien Lerouge'
-MAINTAINER_EMAIL    = 'julien.lerouge@litislab.fr'
-AUTHOR              = 'John Salvatier and Christopher Fonnesbeck'
-AUTHOR_EMAIL        = ''
-URL                 = "http://github.com/pymc-devs/pymc"
+AUTHOR          	= 'Julien Lerouge'
+AUTHOR_EMAIL    	= 'julien.lerouge@litislab.fr'
+URL                 = "https://github.com/jlerouge/crino"
 LICENSE             = "LGPL"
 VERSION             = "1.0.0"
 
@@ -21,16 +19,16 @@ classifiers =  ['Development Status :: 4 - Beta',
                 'Operating System :: OS Independent']
 
 if __name__ == "__main__":
-
-    setup(name = DISTNAME,
-          version = VERSION,
-        maintainer  = MAINTAINER,
-        maintainer_email = MAINTAINER_EMAIL,
-        description = DESCRIPTION,
-        license = LICENSE,
-        url = URL,
-        long_description = LONG_DESCRIPTION,
-        packages = ['crino'],
-        classifiers =classifiers,
-        install_requires=['theano','numpy','scipy'])
-
+setup(
+	name = DISTNAME,
+  	version = VERSION,
+	author = AUTHOR,
+	author_email = AUTHOR_EMAIL,
+    description = DESCRIPTION,
+    license = LICENSE,
+    url = URL,
+    long_description = LONG_DESCRIPTION,
+    packages = ['crino'],
+    classifiers =classifiers,
+    install_requires=['theano','numpy','scipy']
+)
