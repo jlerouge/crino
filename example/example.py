d585a75591a15dc69398d6a1164ca516b6f8acfa
Romain HERAULT <romain.herault@insa-rouen.fr>
2014-12-04 16:19:36 +0100

    update example

--- example/example.py
+++ example/example.py
@@ -43,22 +43,36 @@ if(learn):
     input_pretraining_params={
          'learning_rate': 10.0,
          'batch_size' : 100,
-         'epochs' : 100
+         'epochs' : 3
          }
     output_pretraining_params={
          'learning_rate': 10.0,
          'batch_size' : 100,
-         'epochs' : 50
+         'epochs' : 3
          }    
+    link_pretraining_params={
+         'learning_rate': 10.0,
+         'batch_size' : 100,
+         'epochs' : 3
+         }  
     learning_params={
         'learning_rate' : 2.0,
         'batch_size' : 100,
-        'epochs' : 300,
+        'epochs' : 3,
         'input_pretraining_params' : input_pretraining_params,
-        'output_pretraining_params' : output_pretraining_params 
+        'output_pretraining_params' : output_pretraining_params,
+        'link_pretraining_params' : link_pretraining_params,
+        'link_pretraining' : True
+        
     }
-    hidden_size = 256
-
+    
+    hidden_size = 10
+    inputlayers=[hidden_size, hidden_size]
+    #inputlayers=[]
+    outputlayers=[hidden_size, hidden_size]
+    #outputlayers=[]
+    linklayers=[hidden_size, hidden_size]
+    #linklayers=[]
 print '... loading training data'
 train_set = sio.loadmat('data/train.mat')
 x_train = np.asarray(train_set['x_train'], dtype=theano.config.floatX) # We convert to float32 to 
@@ -72,7 +86,7 @@ print("Image of size %d x %d"%(xSize,xSize))
 # Construct a IODA network on training data
 if(learn):
     print '... building and learning a IODA network'
-    nn = crino.network.InputOutputDeepArchitecture([nFeats, hidden_size], [hidden_size, nFeats], crino.module.Sigmoid)
+    nn = crino.network.DeepNeuralNetwork(nFeats, nFeats, crino.module.Sigmoid, nUnitsInput=inputlayers, nUnitsLink=linklayers, nUnitsOutput=outputlayers)
     nn.linkInputs(T.matrix('x'), nFeats)
     nn.prepare()
     nn.criterion = MeanSquareError(nn.outputs, T.matrix('y'))
