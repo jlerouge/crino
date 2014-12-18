dc4b8a04cba1aac1f415450371cb5209f971c163
Romain HERAULT <romain.herault@insa-rouen.fr>
2014-12-18 15:37:12 +0100

    nettoyage du code example

--- example/example.py
+++ example/example.py
@@ -35,43 +35,79 @@ from crino.network import PretrainedMLP
 from crino.criterion import MeanSquareError
 
 import cPickle as pickle
-
+import json
 import csv
 
-input_pretraining_params={
-        'learning_rate': 10.0,
-        'batch_size' : 100,
-        'epochs' : 300
-        }
-output_pretraining_params={
-        'learning_rate': 10.0,
-        'batch_size' : 100,
-        'epochs' : 300
-        }    
-#link_pretraining_params={
-        #'learning_rate': 1.0,
-        #'batch_size' : 100,
-        #'epochs' : 10
-        #}  
-learning_params={
-    'learning_rate' : 2.0,
-    'batch_size' : 100,
-    'epochs' : 300,
-    'input_pretraining_params' : input_pretraining_params,
-    'output_pretraining_params' : output_pretraining_params,
-    #'link_pretraining_params' : link_pretraining_params,
-    'link_pretraining' : False  
-}
+
+def defaultConfig():
+
+    config={}
     
-hidden_size = 1024
+    #Learning parameters of the input pretraining
+    input_pretraining_params={
+            'learning_rate': 1.0,
+            'batch_size' : 100,
+            'epochs' : 300
+            }
+    
+    #Learning parameters of the output pretraining
+    output_pretraining_params={
+            'learning_rate': 1.0,
+            'batch_size' : 100,
+            'epochs' : 300
+            }
+    
+    #Learning parameters of the link pretraining
+    link_pretraining_params={
+            'learning_rate': 1.0,
+            'batch_size' : 100,
+            'epochs' : 300
+            }
+    
+    #Learning parameters of the supervised training + pretrainings
+    config['learning_params']={
+        'learning_rate' : 1.0,
+        'batch_size' : 100,
+        'epochs' : 300,
+        'input_pretraining_params' : input_pretraining_params,
+        'output_pretraining_params' : output_pretraining_params,
+        'link_pretraining_params' : link_pretraining_params,
+        'link_pretraining' : True
+    }
+    
+    #Size of one hidden representation
+    hidden_size = 1024
+    #Geometry of all hidden representations 
+    config['hidden_geometry'] = [hidden_size,hidden_size]
 
-outfolder='./results/'
+    #How many layers are pretrained (here 1 at input and 1 at output) 
+    config['pretraining_geometry']={
+        'nInputLayers': 1,
+        'nOutputLayers': 1
+    }
 
-exemples=[10,50,100]
-displayepochs=[0,10,100,200,300]
+    #Shall we used known init weights
+    config['init_weights'] = None
+    #Shall we save init weights
+    config['save_init_weights'] = True
+    
+    #Examples to be displayed at testing
+    config['displayed_examples']=[10,50,100]
+
+    #Epochs to be displayed at testing
+    config['displayed_epochs']=[0,10,100,200,300]
+
+    #Where to store results
+    config['outfolder']='./default_config_example_results/'
+    
+    return config
 
 
 class MyPretrainedMLP(PretrainedMLP):
+    
+    def setDisplayedEpochs(self,displayed_epochs):
+        self.displayed_epochs=displayed_epochs        
+    
     def setTestSet(self,x_test,y_test):
         self.shared_x_test=theano.shared(x_test)
         self.shared_y_test=theano.shared(y_test)
@@ -88,7 +124,7 @@ class MyPretrainedMLP(PretrainedMLP):
         
     def checkEpochHook(self):
         self.test_criterion_history.append(np.mean(self.testCriterionFunction()))
-        if self.finetunevars['epoch']+1 in displayepochs:
+        if self.finetunevars['epoch']+1 in self.displayed_epochs:
             self.test_forward_history.append((self.finetunevars['epoch'],self.testForwardFunction()))
             self.app_forward_history.append((self.finetunevars['epoch'],self.appForwardFunction()))
 
@@ -96,8 +132,26 @@ class MyPretrainedMLP(PretrainedMLP):
 def data2greyimg(filename, X):
     Xn=(X-X.min())/(X.max()-X.min())*255
     scipy.misc.imsave(filename, Xn)
+    
 
-def main():   
+def experience(config):
+    
+    needed_params=['learning_params','hidden_geometry','pretraining_geometry','init_weights','save_init_weights','displayed_examples','displayed_epochs','outfolder']
+
+    used_config={}
+    for aParam in needed_params:
+        if not( aParam in config.keys()):
+            raise ValueError("Experience configuration does not contain %s parameter"%(aParam))
+        exec '%s=pickle.loads("""%s""")'%(aParam,pickle.dumps(config[aParam]))
+        used_config[aParam]=config[aParam]
+
+    absoutfolder=os.path.abspath(outfolder)
+    if not os.path.exists(absoutfolder):
+        os.mkdir(absoutfolder)
+    
+    print('... saving used configuration')
+    json.dump(used_config,open(os.path.join(absoutfolder,"configuration.json"),'wb'),indent=2)
+    
     print '... loading training data'
     train_set = sio.loadmat('data/fixed/train.mat')
     x_train = np.asarray(train_set['x_train'], dtype=theano.config.floatX) # We convert to float32 to 
@@ -114,93 +168,75 @@ def main():
     nFeats = x_train.shape[1] # number of pixels per image
     xSize = int(np.sqrt(nFeats)) # with of a square image
 
+    # Input representation size is the number of pixel
     nInputs=nFeats
+    # Output representation size is the number of pixel
     nOutputs=nFeats
     
-    geometry=[nFeats,hidden_size,hidden_size,nFeats]
+    # Compute the full geometry of the MLP
+    geometry=[nFeats] + hidden_geometry+[nFeats]
+    # Compute the number of layers
     nLayers=len(geometry)-1
-
-    # All configurations have the same geometry 3 layers and 4 representations
-    # with sizes [nFeats,hidden_size,hidden_size,nFeats].
-    # They only differ in the way layers are pretrained or not.
-    configurations=[]
-    # Standard MLP, no pretraining
-    configurations.append({'nInputLayers':0,'nOutputLayers':0})
-    # First layer pretrained input way
-    configurations.append({'nInputLayers':1,'nOutputLayers':0})
-    # Two first layers pretrained input way
-    configurations.append({'nInputLayers':2,'nOutputLayers':0})
-    # Last layer pretrained output way
-    configurations.append({'nInputLayers':0,'nOutputLayers':1})
-    # Two last layers pretrained output way
-    configurations.append({'nInputLayers':0,'nOutputLayers':2})
-    # First layer pretrained input way, and last layer pretrained output way
-    configurations.append({'nInputLayers':1,'nOutputLayers':1})
-
-    parameters=None
-    #We throw random parameters only for the first conf and then reuse the same parameters for the remaining confs.
-
-    absoutfolder=os.path.abspath(outfolder)
-    if not os.path.exists(absoutfolder):
-        os.mkdir(absoutfolder)
 
     results={}
   
     for phase,xdata,ydata in [['train',x_train,y_train],['test',x_test,y_test]]:  
-        for ex in exemples:
+        for ex in displayed_examples:
             x_orig = np.reshape(xdata[ex:ex+1], (xSize, xSize), 'F')
             data2greyimg(os.path.join(absoutfolder,"%s_ex_%03d_input.png"%(phase,ex,)),x_orig)
             y_true = np.reshape(ydata[ex:ex+1], (xSize, xSize), 'F')
             data2greyimg(os.path.join(absoutfolder,"%s_ex_%03d_target.png"%(phase,ex,)),y_true)
 
-    for conf in configurations:
-        
-        expname="I_%d_L_%d_O_%d"%(conf['nInputLayers'],nLayers-conf['nInputLayers']-conf['nOutputLayers'],conf['nOutputLayers'])
-        print '... building and learning a network %s'%(expname,)
-        nn = MyPretrainedMLP(geometry, outputActivation=crino.module.Sigmoid,**conf)
-        nn.setTestSet(x_test,y_test)
-        nn.linkInputs(T.matrix('x'), nFeats)
-        nn.prepare()
-        nn.criterion = MeanSquareError(nn.outputs, T.matrix('y'))
-        if parameters is None:
-            parameters=nn.getParameters()
-            pickle.dump(parameters,open(os.path.join(absoutfolder,"starting_params.pck"),'w'),protocol=-1)
-        else:
-            nn.setParameters(parameters)
-        delta = nn.train(x_train, y_train, **learning_params)
-        print '... learning lasted %s (s) ' % (delta)
-        
-        results[expname]={
-            'I':conf['nInputLayers'],
-            'L':nLayers-conf['nInputLayers']-conf['nOutputLayers'],
-            'O':conf['nOutputLayers'],
-            'train_criterion':nn.finetunevars['history'][-1],
-            'train_history':nn.finetunevars['history'],
-            'train_full_history':nn.finetunevars['full_history'],
-            'test_criterion': nn.test_criterion_history[-1],
-            'test_history':nn.test_criterion_history,
-            }
-        pickle.dump(nn.getParameters(),open(os.path.join(absoutfolder,"%s_params.pck"%(expname,)),'w'),protocol=-1)
-        
-        for phase,xdata,ydata,history in [
-                    ['train',x_train,y_train,nn.app_forward_history],
-                    ['test',x_test,y_test,nn.test_forward_history]]:
-            for ex in exemples:
-                for epoch,forward in history:
-                    y_estim = np.reshape(forward[ex:ex+1], (xSize, xSize), 'F')
-                    data2greyimg(os.path.join(absoutfolder,"%s_%s_ex_%03d_estim_%03d.png"%(expname,phase,ex,epoch+1)),y_estim)
+
+    print '... building and learning a network'
+    nn = MyPretrainedMLP(geometry, outputActivation=crino.module.Sigmoid,**pretraining_geometry)
+    
+    nn.setTestSet(x_test,y_test)
+    nn.setDisplayedEpochs(displayed_epochs)
+    
+    nn.linkInputs(T.matrix('x'), nFeats)
+    nn.prepare()
+    nn.criterion = MeanSquareError(nn.outputs, T.matrix('y'))
+    if not(init_weights is None):
+        nn.setParameters(init_weights)
+    if save_init_weights:
+        pickle.dump(init_weights,open(os.path.join(absoutfolder,"starting_params.pck"),'w'),protocol=-1)
+
+    delta = nn.train(x_train, y_train, **learning_params)
+    print '... learning lasted %s (s) ' % (delta)
+    
+    results={
+        'I':pretraining_geometry['nInputLayers'],
+        'L':nLayers-pretraining_geometry['nInputLayers']-pretraining_geometry['nOutputLayers'],
+        'O':pretraining_geometry['nOutputLayers'],
+        'train_criterion':nn.finetunevars['history'][-1],
+        'train_history':nn.finetunevars['history'],
+        'train_full_history':nn.finetunevars['full_history'],
+        'test_criterion': nn.test_criterion_history[-1],
+        'test_history':nn.test_criterion_history,
+        }
+    pickle.dump(nn.getParameters(),open(os.path.join(absoutfolder,"learned_params.pck"),'w'),protocol=-1)
+    
+    for phase,xdata,ydata,history in [
+                ['train',x_train,y_train,nn.app_forward_history],
+                ['test',x_test,y_test,nn.test_forward_history]]:
+        for ex in displayed_examples:
+            for epoch,forward in history:
+                y_estim = np.reshape(forward[ex:ex+1], (xSize, xSize), 'F')
+                data2greyimg(os.path.join(absoutfolder,"%s_ex_%03d_estim_%03d.png"%(phase,ex,epoch+1)),y_estim)
                     
     pickle.dump(results,open(os.path.join(absoutfolder,'results.pck'),'w'),protocol=-1)  
     
     table=[["Input Pretrained Layers","Link Layers","Output Pretrained Layers", "Epoch","Train", "Test"]]
-    for expname in results.keys():
-        for epoch in displayepochs:
-            table.append([results[expname]['I'],results[expname]['L'],results[expname]['O'],epoch,results[expname]['train_history'][epoch],results[expname]['test_history'][epoch]])
+    for epoch in displayed_epochs:
+        table.append([results['I'],results['L'],results['O'],epoch,results['train_history'][epoch],results['test_history'][epoch]])
 
     writer=csv.writer(open(os.path.join(absoutfolder,'results.csv'),'wb'),delimiter='\t')
     for row in table:
         writer.writerow(row)
-  
+
+def main():
+    experience(defaultConfig())
 
 if __name__=="__main__":
     main()
